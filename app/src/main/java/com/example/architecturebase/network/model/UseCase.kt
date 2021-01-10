499e398336343a5f4647811fb30b82a64435640b
PavelShramko <pavel.shramko.97@mail.ru>
2021-01-10 22:21:10 +0300

    Comment

--- app/src/main/java/com/example/architecturebase/network/model/UseCase.kt
+++ app/src/main/java/com/example/architecturebase/network/model/UseCase.kt
@@ -1,7 +1,7 @@
 package com.example.architecturebase.network.model
 
 class UseCase {
-    //Правильно понял, когда сказал, что нужно вынести логику в отдельный UseCase?
+    //Правильно понял, что имелось в виду под вынести логику в отдельный UseCase?
 
     fun createUseCase(posts: List<Post>):  List<Post>{
         return posts.filter {
